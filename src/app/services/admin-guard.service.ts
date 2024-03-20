d033123b08c90487c7ae54c09521a943a5af6058
Hrissy-art <hristinastankova89@gmail.com>
2024-03-20 22:38:37 +0100

    major changes

---
+++ src/app/services/admin-guard.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+  CanActivateFn,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+import { UserStoreService } from './user-store.service';
+import { Observable, of, switchMap, map } from 'rxjs';
+
+export const adminGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+): Observable<boolean | UrlTree> => {
+  const authService = inject(AuthService);
+  console.log('authService', 'router', 'userStoreService');
+
+  const router = inject(Router);
+  const userStoreService = inject(UserStoreService);
+
+  // const userId = authService.getUserIdFromToken();
+  // console.log("ID de l'utilisateur connecté:", userId);
+
+  // Récupérez les rôles du token
+  const rolesFromToken = authService.getRoleFromToken();
+
+  return userStoreService.getRolesFromStore().pipe(
+    switchMap((roles) => {
+      console.log('Rôles récupérés du store:', roles);
+
+      // Vérifiez si les rôles du token incluent 'ROLE_ADMIN'
+      if (rolesFromToken.includes('ROLE_ADMIN')) {
+        console.log("L'utilisateur est un administrateur");
+        return authService.isLoggedInObservable().pipe(
+          map((loggedIn) => {
+            if (loggedIn) {
+              return true;
+            } else {
+              return router.createUrlTree(['/auth']);
+            }
+          })
+        );
+      } else {
+        console.log("L'utilisateur n'est pas un administrateur");
+        return of(router.createUrlTree(['/auth']));
+      }
+    })
+  );
+};
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,6 @@
 // import { map, tap } from 'rxjs/operators';
 // import AuthLogin from './authLogin';
 
-
 // @Injectable({
 //   providedIn: 'root'
 // })
@@ -19,7 +18,7 @@
 //     .pipe(
 //         map(value => value.token)
 //         )
-//     .subscribe(value => 
+//     .subscribe(value =>
 //       {
 //         localStorage.setItem('token', value);
 //       });
@@ -38,7 +37,7 @@
 //   logout(): Observable<any> {
 
 //     localStorage.removeItem('');
-    
+
 //     return this.http.post(`${this.apiUrl}/logout`, {});
 //   }
 // }
@@ -60,28 +59,76 @@
 //         }
 
 //         storeToken(token: string): void {
-            
+
 //         }
 //     }
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { TokenService } from "./token.service";
-import { Token } from "@angular/compiler";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { TokenService } from './token.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserStoreService } from './user-store.service';
 
 @Injectable({
-          providedIn: 'root'
-        })
-        export class AuthService {
+  providedIn: 'root',
+})
+export class AuthService {
+  private userPayload: any;
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+    private userStore: UserStoreService
+  ) {
+    this.userPayload = this.decodeToken();
+  }
+  login(email: string, password: string): Observable<any> {
+    return this.http.post('http://localhost:8000/api/login_check', {
+      username: email,
+      password: password,
+    });
+  }
+  storeToken(token: string): void {
+    this.tokenService.setToken(token);
+  }
+  // vérifier si l'utilisateur se connecter à travers l'existance de token
+  isLoggedIn(): boolean {
+    return this.tokenService.isLogged();
+  }
 
-            
-            constructor(private http: HttpClient, private tokenService: TokenService ) {} 
-            login(email: string, password: string): Observable<any> {
-                return this.http.post('http://localhost:8000/api/login_check', {
-                    username: email,
-                    password: password,
-                });
-            }
-            storeToken (token:string): void {
-                this.tokenService.setToken(token);
-            }}
+  isLoggedInObservable(): Observable<boolean> {
+    const isLoggedIn = this.isLoggedIn();
+    return of(isLoggedIn);
+  }
+
+  logout(): void {
+    this.tokenService.clearToken();
+  }
+
+  decodeToken() {
+    const jwtHelper = new JwtHelperService();
+    const token = this.tokenService.getToken()!;
+    const decodedToken = jwtHelper.decodeToken(token);
+    console.log('Token décodé:', decodedToken);
+    return jwtHelper.decodeToken(token);
+  }
+
+  // getfullnameFromToken() {
+  //   if (this.userPayload) return this.userPayload.name;
+  // }
+
+  getRoleFromToken() {
+    const userPayload = this.decodeToken();
+    return userPayload?.roles || [];
+  }
+
+  setUserDetailsInStore() {
+    const roles = this.getRoleFromToken();
+    console.log('Rôles extraits du token:', roles);
+    this.userStore.setRolesFromStore(roles);
+  }
+
+  getUserIdFromToken() {
+    const userPayload = this.decodeToken();
+    return userPayload?.user_id; // Assurez-vous que votre token contient l'ID de l'utilisateur sous la clé 'user_id'
+  }
+}
