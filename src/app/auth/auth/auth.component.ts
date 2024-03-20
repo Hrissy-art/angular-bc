5076bff8b71626111e57b975a02db4c5e12228b1
Hrissy-art <hristinastankova89@gmail.com>
2024-03-20 22:16:05 +0100

    category update

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -6,32 +6,37 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, private router: Router) {}
 
   isLoginMode = true;
 
   onSwitchMode() {
-
     this.isLoginMode = !this.isLoginMode;
   }
 
-  onSubmit(form: NgForm){
-// console.log(form.value);
-// form.reset();
-  if (!form.valid) { 
-    return;
+  onSubmit(form: NgForm) {
+    // console.log(form.value);
+    // form.reset();
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.authService
+      .login(email, password)
+      .subscribe((res: { token: string }) => {
+        this.authService.storeToken(res.token);
+        this.router.navigate(['/home']);
+      });
   }
 
-  const email = form.value.email;
-  const password = form.value.password;
-
-  this.authService.login(email, password).subscribe((res: { token: string; })=>{
-    this.authService.storeToken(res.token);
-    this.router.navigate(['/home']);
-  });
-}
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,65 +1,15 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Product } from '../models/products';
-// import { CartService } from '../services/cart.service';
-// import { Service } from '../models/service';
-// import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-cart',
-//   templateUrl: './cart.component.html',
-//   styleUrl: './cart.component.css',
-// })
-// export class CartComponent implements OnInit {
-//   cart: Product[] = [];
-//   selectedServices: Service[] = [];
-
-//   constructor(private cartService: CartService, private router: Router) {}
-
-//   ngOnInit(): void {
-//     this.cart = this.cartService.getCart();
-//     this.selectedServices = this.cartService.getSelectedServices(); // Récupérer les services sélectionnés
-//   }
-
-//   removeFromCart(index: number): void {
-//     this.cartService.removeFromCart(index);
-//     this.cart = this.cartService.getCart(); // Mettez à jour le panier après la suppression d'un produit
-//   }
-//   getTotalPrice(): number {
-//     let totalPrice = 0;
-
-//     // Ajouter le prix des produits au total
-//     totalPrice += this.cart.reduce(
-//       (total, product) => total + product.price,
-//       0
-//     );
-
-//     // Ajouter le prix des services au total
-//     totalPrice += this.selectedServices.reduce(
-//       (total, service) => total + service.coeff,
-//       0
-//     );
-
-//     return totalPrice;
-//   }
-//   removeSelectedServices(): void {
-//     // Supprimer les services sélectionnés du panier
-//     this.cartService.removeServicesFromCart();
-//     // Réinitialiser la liste des services sélectionnés
-//     this.selectedServices = [];
-//   }
-
-//   // Méthode pour valider le panier et naviguer vers une autre page
-//   validateCart(): void {
-//     // Après avoir validé, vous pouvez naviguer vers une autre page
-//     this.router.navigate(['/orderDate']);
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from '../models/products';
 import { CartService } from '../services/cart.service';
 import { Service } from '../models/service';
 import { Material } from '../models/material';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { OrderService } from '../services/order.service';
+import { Order } from '../models/order';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-cart',
@@ -67,39 +17,104 @@ import { Router } from '@angular/router';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  formGroup: FormGroup;
+  orderId: string = '';
+  statusOptions = [
+    { label: 'En attente', value: '/api/status_orders/1' },
+    { label: 'En cours', value: '/api/status_orders/2' },
+    { label: 'Terminée', value: '/api/status_orders/3' },
+  ];
+
   cart: Product[] = [];
   selectedServices: Service[] = [];
   selectedMaterials: Material[] = [];
+  private selectedServiceIds: number[] = [];
+  private selectedMaterialIds: number[] = [];
+  showButton: boolean = false;
+  formSubmitted: boolean = false;
+  userId: string = '';
+  showAlert: boolean = false;
 
-  constructor(private cartService: CartService, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private orderService: OrderService,
+    private cartService: CartService,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.userId = localStorage.getItem('userId') || '';
+    this.formGroup = this.formBuilder.group({
+      dateOrder: ['', Validators.required],
+      dateRender: ['', Validators.required],
+      client: [
+        this.userId ? `/api/clients/${this.userId}` : '', // Utilisez l'ID de l'utilisateur pour pré-remplir le champ "Client" si disponible dans le stockage local
+        Validators.required,
+      ],
+      statusOrder: ['/api/status_orders/1', Validators.required], // Mettre '/api/status_orders/1' par défaut
+    });
+  }
 
   ngOnInit(): void {
     this.cart = this.cartService.getCart();
-    this.selectedServices = this.cartService.getSelectedServices(); // Récupérer les services sélectionnés
-    this.selectedMaterials = this.cartService.getSelectedMaterials(); // Récupérer les matériaux sélectionnés
+    this.selectedServices = this.cartService.getSelectedServices();
+    this.selectedMaterials = this.cartService.getSelectedMaterials();
+    this.updateSelectedIds();
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.valid && this.cart.length > 0) {
+      if (this.isAuthenticated()) {
+        const formData = this.formGroup.value as Order;
+        this.createOrder(formData).subscribe(
+          (response) => {
+            console.log('Commande envoyée avec succès !', response);
+            if (response.id !== null && response.id !== undefined) {
+              this.orderId = response.id;
+              localStorage.setItem('orderId', this.orderId);
+              const storedOrderId = localStorage.getItem('orderId');
+              console.log(
+                'ID de commande sauvegardé dans le local storage :',
+                storedOrderId
+              );
+            }
+            this.formSubmitted = true;
+          },
+          (error) => {
+            console.error("Erreur lors de l'envoi de la commande:", error);
+          }
+        );
+      } else {
+        // Rediriger vers la page de connexion
+        this.router.navigate(['/auth']);
+      }
+    } else {
+      console.log('Le formulaire est invalide ou le panier est vide.');
+    }
+  }
+
+  createOrder(order: Order): Observable<any> {
+    return this.orderService.createOrder(order);
   }
 
   removeFromCart(index: number): void {
     this.cartService.removeFromCart(index);
-    this.cart = this.cartService.getCart(); // Mettez à jour le panier après la suppression d'un produit
+    this.updateCart();
   }
 
   getTotalPrice(): number {
     let totalPrice = 0;
 
-    // Ajouter le prix des produits au total
     totalPrice += this.cart.reduce(
       (total, product) => total + product.price,
       0
     );
 
-    // Ajouter le prix des services au total
     totalPrice += this.selectedServices.reduce(
       (total, service) => total + service.coeff,
       0
     );
 
-    // Ajouter le prix des matériaux au total
     totalPrice += this.selectedMaterials.reduce(
       (total, material) => total + material.coeff,
       0
@@ -109,22 +124,238 @@ export class CartComponent implements OnInit {
   }
 
   removeSelectedServices(): void {
-    // Supprimer les services sélectionnés du panier
     this.cartService.removeServicesFromCart();
-    // Réinitialiser la liste des services sélectionnés
-    this.selectedServices = [];
+    this.updateCart();
   }
 
   removeSelectedMaterials(): void {
-    // Supprimer les matériaux sélectionnés du panier
     this.cartService.removeMaterialsFromCart();
-    // Réinitialiser la liste des matériaux sélectionnés
-    this.selectedMaterials = [];
+    this.updateCart();
   }
 
-  // Méthode pour valider le panier et naviguer vers une autre page
   validateCart(): void {
-    // Après avoir validé, vous pouvez naviguer vers une autre page
-    this.router.navigate(['/orderDate']);
+    const productIds = this.cart.map((product) => product.id);
+    const productUrls = productIds.map((id) => `/api/products/${id}`);
+
+    const serviceUrls = this.selectedServiceIds.map(
+      (id) => `/api/services/${id}`
+    );
+    const materialUrls = this.selectedMaterialIds.map(
+      (id) => `/api/materials/${id}`
+    );
+
+    this.router.navigate(['/form'], {
+      state: {
+        productUrls,
+        serviceUrls,
+        materialUrls,
+      },
+    });
+
+    // this.router.navigateByUrl('/form', {
+    //   state: { productUrls, serviceUrls, materialUrls },
+    // });
+    this.showButton = true;
+  }
+
+  private updateCart(): void {
+    this.cart = this.cartService.getCart();
+    this.selectedServices = this.cartService.getSelectedServices();
+    this.selectedMaterials = this.cartService.getSelectedMaterials();
+    this.updateSelectedIds();
+  }
+
+  private updateSelectedIds(): void {
+    this.selectedServiceIds = this.selectedServices.map(
+      (service) => service.id
+    );
+    this.selectedMaterialIds = this.selectedMaterials.map(
+      (material) => material.id
+    );
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  redirectToAnotherPage(): void {
+    // Naviguer vers une autre page lorsqu'on clique sur le bouton
+    this.router.navigate(['/client']);
+    this.showAlert = true;
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Product } from '../models/products';
+// import { CartService } from '../services/cart.service';
+// import { Service } from '../models/service';
+// import { Material } from '../models/material';
+// import { Router } from '@angular/router';
+// import { HttpClient } from '@angular/common/http';
+// import { OrderService } from '../services/order.service';
+// import { Order } from '../models/order';
+// import { Observable } from 'rxjs';
+// import { AuthService } from '../services/auth.service';
+
+// @Component({
+//   selector: 'app-cart',
+//   templateUrl: './cart.component.html',
+//   styleUrls: ['./cart.component.css'],
+// })
+// export class CartComponent implements OnInit {
+//   formGroup: FormGroup;
+//   orderId: string = '';
+//   statusOptions = [
+//     { label: 'En attente', value: '/api/status_orders/1' },
+//     { label: 'En cours', value: '/api/status_orders/2' },
+//     { label: 'Terminée', value: '/api/status_orders/3' },
+//   ];
+
+//   cart: Product[] = [];
+//   selectedServices: Service[] = [];
+//   selectedMaterials: Material[] = [];
+//   private selectedServiceIds: number[] = [];
+//   private selectedMaterialIds: number[] = [];
+//   showButton: boolean = false;
+//   formSubmitted: boolean = false;
+
+//   constructor(
+//     private formBuilder: FormBuilder,
+//     private http: HttpClient,
+//     private orderService: OrderService,
+//     private cartService: CartService,
+//     private router: Router,
+//     private authService: AuthService
+//   ) {
+//     this.formGroup = this.formBuilder.group({
+//       dateOrder: ['', Validators.required],
+//       dateRender: ['', Validators.required],
+//       client: ['', Validators.required],
+//       statusOrder: ['', Validators.required],
+//     });
+//   }
+
+//   ngOnInit(): void {
+//     this.cart = this.cartService.getCart();
+//     this.selectedServices = this.cartService.getSelectedServices();
+//     this.selectedMaterials = this.cartService.getSelectedMaterials();
+//     this.updateSelectedIds();
+//   }
+
+//   onSubmit(): void {
+//     if (this.formGroup.valid && this.cart.length > 0) {
+//       if (this.isAuthenticated()) {
+//         const formData = this.formGroup.value as Order;
+//         this.createOrder(formData).subscribe(
+//           (response) => {
+//             console.log('Commande envoyée avec succès !', response);
+//             if (response.id !== null && response.id !== undefined) {
+//               this.orderId = response.id;
+//               localStorage.setItem('orderId', this.orderId);
+//               const storedOrderId = localStorage.getItem('orderId');
+//               console.log(
+//                 'ID de commande sauvegardé dans le local storage :',
+//                 storedOrderId
+//               );
+//             }
+//             this.formSubmitted = true;
+//           },
+//           (error) => {
+//             console.error("Erreur lors de l'envoi de la commande:", error);
+//           }
+//         );
+//       } else {
+//         // Rediriger vers la page de connexion
+//         this.router.navigate(['/auth']);
+//       }
+//     } else {
+//       console.log('Le formulaire est invalide ou le panier est vide.');
+//     }
+//   }
+
+//   createOrder(order: Order): Observable<any> {
+//     return this.orderService.createOrder(order);
+//   }
+
+//   removeFromCart(index: number): void {
+//     this.cartService.removeFromCart(index);
+//     this.updateCart();
+//   }
+
+//   getTotalPrice(): number {
+//     let totalPrice = 0;
+
+//     totalPrice += this.cart.reduce(
+//       (total, product) => total + product.price,
+//       0
+//     );
+
+//     totalPrice += this.selectedServices.reduce(
+//       (total, service) => total + service.coeff,
+//       0
+//     );
+
+//     totalPrice += this.selectedMaterials.reduce(
+//       (total, material) => total + material.coeff,
+//       0
+//     );
+
+//     return totalPrice;
+//   }
+
+//   removeSelectedServices(): void {
+//     this.cartService.removeServicesFromCart();
+//     this.updateCart();
+//   }
+
+//   removeSelectedMaterials(): void {
+//     this.cartService.removeMaterialsFromCart();
+//     this.updateCart();
+//   }
+
+//   validateCart(): void {
+//     const productIds = this.cart.map((product) => product.id);
+//     const productUrls = productIds.map((id) => `/api/products/${id}`);
+
+//     const serviceUrls = this.selectedServiceIds.map(
+//       (id) => `/api/services/${id}`
+//     );
+//     const materialUrls = this.selectedMaterialIds.map(
+//       (id) => `/api/materials/${id}`
+//     );
+
+//     this.router.navigate(['/form'], {
+//       state: {
+//         productUrls,
+//         serviceUrls,
+//         materialUrls,
+//       },
+//     });
+
+//     // this.router.navigateByUrl('/form', {
+//     //   state: { productUrls, serviceUrls, materialUrls },
+//     // });
+//     this.showButton = true;
+//   }
+
+//   private updateCart(): void {
+//     this.cart = this.cartService.getCart();
+//     this.selectedServices = this.cartService.getSelectedServices();
+//     this.selectedMaterials = this.cartService.getSelectedMaterials();
+//     this.updateSelectedIds();
+//   }
+
+//   private updateSelectedIds(): void {
+//     this.selectedServiceIds = this.selectedServices.map(
+//       (service) => service.id
+//     );
+//     this.selectedMaterialIds = this.selectedMaterials.map(
+//       (material) => material.id
+//     );
+//   }
+
+//   isAuthenticated(): boolean {
+//     return this.authService.isLoggedIn();
+//   }
+// }
